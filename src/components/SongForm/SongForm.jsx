62eb7b3d69dacc2febaa162c0d3b8d9bbd81e486
Daniel Tulpa <dtulpa16@yahoo.com>
2022-01-07 19:09:45 -0600

    Changed State In Song Form Component To A Song Object

--- src/components/SongForm/SongForm.jsx
+++ src/components/SongForm/SongForm.jsx
@@ -3,22 +3,19 @@ import './SongForm.css'
 import axios from "axios";
 
 const SongForm = (props) => {
-    const [title,setTitle] = useState('')
-    const [artist,setArtist] = useState('')
-    const [album,setAlbum] = useState('')
-    const [genre,setGenre] = useState('')
-    const [release_date,setReleaseDate] = useState('')
+    const [state, setState] = useState({ title: "", artist: "", album:"", genre:"", release_date:""});
+    
+        const handleChange = e => {
+            const { name, value } = e.target;
+            setState(prevState => ({
+                ...prevState,
+                [name]: value
+            }));
+        };
 
 const handleSubmit = async(e) =>{
     e.preventDefault()
-    let newSong = {
-        title:title,
-        artist:artist,
-        album:album,
-        genre: genre,
-        release_date:release_date
-    }
-    await axios.post('http://127.0.0.1:8000/music/', newSong)
+    await axios.post('http://127.0.0.1:8000/music/', state)
     props.newSong()
 }
 
@@ -27,15 +24,15 @@ const handleSubmit = async(e) =>{
             <form onSubmit={handleSubmit}>
                 <h1>Add a Song</h1>
                 <label>Title</label>
-                <input name="title" onChange={(e)=>setTitle(e.target.value)} />
+                <input name="title" onChange={handleChange} />
                 <label>Artist</label>
-                <input name="artist" onChange={(e)=>setArtist(e.target.value)} />
+                <input name="artist" onChange={handleChange} />
                 <label>Album</label>
-                <input name="album" onChange={(e)=>setAlbum(e.target.value)} />
+                <input name="album" onChange={handleChange} />
                 <label>Genre</label>
-                <input name="genre" onChange={(e)=>setGenre(e.target.value)} />
+                <input name="genre" onChange={handleChange} />
                 <label>Release Date</label>
-                <input type="date" name="release_date" onChange={(e)=>setReleaseDate(e.target.value)} />
+                <input type="date" name="release_date" onChange={handleChange} />
                 <button type="submit">Add Song</button>
             </form>
             </div>
